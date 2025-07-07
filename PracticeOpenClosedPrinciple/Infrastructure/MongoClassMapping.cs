441247560be0344ab85b912100d3453608d2ae17
Mehran Abdi <mehranabdi13793@gmail.com>
2025-07-07 10:57:49 +0330

    Add mongodb class mapper

---
+++ PracticeOpenClosedPrinciple/Infrastructure/MongoClassMapping.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Serializers;
+using PracticeOpenClosedPrinciple.Model;
+
+namespace PracticeOpenClosedPrinciple.Infrastructure;
+
+public static class MongoClassMapping
+{
+    private static bool _initialized;
+
+    public static void RegisterClassMaps()
+    {
+        if (_initialized) return;
+
+        if (!BsonClassMap.IsClassMapRegistered(typeof(BaseEntity)))
+            BsonClassMap.RegisterClassMap<BaseEntity>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapIdMember(c => c.Id)
+                    .SetSerializer(new GuidSerializer(BsonType.String)) // store GUIDs as strings (safer for querying)
+                    .SetIdGenerator(GuidGenerator.Instance);
+            });
+
+        if (!BsonClassMap.IsClassMapRegistered(typeof(Contact)))
+            BsonClassMap.RegisterClassMap<Contact>(cm => { cm.AutoMap(); });
+
+        _initialized = true;
+    }
+}
