83cdf4a34f66584c8acf64290badd1a4b84cd2d7
Mehran Abdi <mehranabdi13793@gmail.com>
2025-07-07 10:58:03 +0330

    Add MongoDb Context

---
+++ PracticeOpenClosedPrinciple/Infrastructure/MongoDbContext.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using PracticeOpenClosedPrinciple.Model;
+
+namespace PracticeOpenClosedPrinciple.Infrastructure;
+
+public class MongoDbContext<T> where T : BaseEntity
+{
+    private readonly IMongoCollection<T> _collection;
+
+    public MongoDbContext()
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("ContactAppDb");
+        _collection = database.GetCollection<T>(typeof(T).Name);
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _collection.InsertOneAsync(entity);
+        return entity;
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        await _collection.ReplaceOneAsync(a => a.Id == entity.Id, entity);
+        return entity;
+    }
+
+    public async Task<T> DeleteAsync(T entity)
+    {
+        await _collection.DeleteOneAsync(a => a.Id == entity.Id);
+        return entity;
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return (await _collection.FindAsync(_ => true)).ToList();
+    }
+}
---
+++ PracticeOpenClosedPrinciple/Services/UpdateContactFunction.cs
@@ -0,0 +1,35 @@
+﻿using PracticeOpenClosedPrinciple.Infrastructure;
+using PracticeOpenClosedPrinciple.Model;
+
+namespace PracticeOpenClosedPrinciple.Services;
+
+public class UpdateContactFunction : IContactFunction
+{
+    private readonly MongoDbContext<Contact> _db;
+
+    public UpdateContactFunction(MongoDbContext<Contact> db)
+    {
+        _db = db;
+    }
+
+    public string OptionCode => "3";
+    public string Description => "Updated contact";
+
+    public async Task Action()
+    {
+        Console.Write("Please enter name: ");
+        var name = Console.ReadLine();
+        var updateContact = (await _db.GetAllAsync()).FirstOrDefault(c => c.Name == name);
+        while (updateContact == null)
+        {
+            Console.WriteLine("Contact does not exist");
+            return;
+        }
+
+        Console.Write("Please enter phone number: ");
+        var phoneNumber = Console.ReadLine() ?? updateContact.Phone;
+        updateContact.Phone = phoneNumber;
+        await _db.UpdateAsync(updateContact);
+        Console.WriteLine("Contact updated");
+    }
+}
