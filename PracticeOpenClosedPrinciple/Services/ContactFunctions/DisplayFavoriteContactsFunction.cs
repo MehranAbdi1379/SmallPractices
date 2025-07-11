6bb3f22a550224f92aaf4df6aafedab5c657faf1
Mehran Abdi <mehranabdi13793@gmail.com>
2025-07-11 12:11:03 +0330

    Add Display Favorite contacts function

---
+++ PracticeOpenClosedPrinciple/Services/ContactFunctions/DisplayFavoriteContactsFunction.cs
@@ -0,0 +1,31 @@
+﻿using PracticeOpenClosedPrinciple.Infrastructure;
+using PracticeOpenClosedPrinciple.Model;
+
+namespace PracticeOpenClosedPrinciple.Services.ContactFunctions;
+
+public class DisplayFavoriteContactsFunction : IContactFunction
+{
+    private readonly MongoDbContext<Contact> _db;
+
+    public DisplayFavoriteContactsFunction(MongoDbContext<Contact> db)
+    {
+        _db = db;
+    }
+
+    public string OptionCode => "8";
+    public string Description => "Display favorite contacts";
+
+    public async Task Action()
+    {
+        var contacts = _db.GetQueryable().Where(c => c.Favorite).ToList();
+
+        if (contacts.Count == 0)
+        {
+            Console.WriteLine("There are no contacts");
+            return;
+        }
+
+        foreach (var contact in contacts)
+            Console.WriteLine($"{contact.Name}: {contact.Phone}.");
+    }
+}
