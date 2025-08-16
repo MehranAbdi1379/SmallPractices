6b4f1e4954227a31f815d436245131d665b30ba8
MehranAbdi1379 <mehranabdi13793@gmail.com>
2025-08-16 11:26:37 +0330

    Practice Generic Variance

---
+++ GenericVariance/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GenericVariance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IList<string> strings = new List<string>();
+        IList<object> objects = strings;
+    }
+}
+
+public class Animal
+{
+}
+
+public class Dog : Animal
+{
+}
