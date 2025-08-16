dbc6f89c2a213d7dd030b0690daf5a110926ed3c
MehranAbdi1379 <mehranabdi13793@gmail.com>
2025-08-16 11:25:28 +0330

    Add async practice application

---
+++ AsynchoronousProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AsynchoronousProgramming;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        _ = DoSomeAsyncStuff2();
+        Console.WriteLine("Third Ended");
+        await Task.Delay(3000);
+    }
+
+    private static async Task DoSomeAsyncStuff()
+    {
+        await Task.Delay(2000);
+        Console.WriteLine("First Ended");
+    }
+
+    private static async Task DoSomeAsyncStuff2()
+    {
+        DoSomeAsyncStuff();
+        Console.WriteLine("Second Ended");
+    }
+}
--- SmallPractices/obj/Debug/net8.0/SmallPractices.AssemblyInfo.cs
+++ SmallPractices/obj/Debug/net8.0/SmallPractices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmallPractices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a6b14214b9efed5c0b23820f5fc1c746241aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288b292dee19880392df64c47773288643c85d0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmallPractices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmallPractices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
